9874a0eebb2f2100faaa47697de878e9e844cc20
toilari <ilari97@hotmail.com>
2020-03-09 07:05:52 +0200

    Initial commit

---
+++ src/main/java/caves/Main.java
@@ -0,0 +1,14 @@
+package caves;
+
+public final class Main {
+    private Main() {
+    }
+
+    /**
+     * Application main entry-point.
+     * @param args un-parsed command-line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/caves/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes required for bootstrapping the application. Root package for the project.
+ */
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package caves;
+
+import caves.util.annotation.FieldsAreNonnullByDefault;
+import caves.util.annotation.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
