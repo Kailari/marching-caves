ffa4f5b92f5934a06c33abb801a035d4dd8733a3
toilari <ilari97@hotmail.com>
2020-03-11 08:10:23 +0200

    Added more placeholders for rest of the graphics pipeline

---
+++ src/main/java/caves/window/rendering/GraphicsPipeline.java
@@ -0,0 +1,4 @@
+package caves.window.rendering;
+
+public class GraphicsPipeline {
+}
--- src/main/java/caves/window/rendering/RenderingContext.java
+++ src/main/java/caves/window/rendering/RenderingContext.java
@@ -2,30 +2,17 @@ package caves.window.rendering;
 
 import caves.window.DeviceContext;
 
+import static org.lwjgl.vulkan.VK10.vkDeviceWaitIdle;
+
 public final class RenderingContext implements AutoCloseable {
+    private final DeviceContext deviceContext;
+
     private final SwapChain swapChain;
+    private final GraphicsPipeline graphicsPipeline;
+    private final Framebuffers framebuffers;
+    private final CommandBuffers commandBuffers;
 
     private boolean mustRecreateSwapChain;
-
-    /**
-     * Gets the swapchain and re-creates it in case the current instance has been invalidated. The
-     * result is considered invalid after GLFW events have been polled. Thus, the intended usage is
-     * to call get the swapchain once, immediately after polling the events:
-     * <pre><code>
-     *     glfwPollEvents();
-     *     final var swapchain = context.getSwapChain();
-     * </code></pre>
-     *
-     * @return the current swapchain instance, which is guaranteed to be valid
-     */
-    public SwapChain getSwapChain() {
-        if (this.mustRecreateSwapChain) {
-            //this.swapChain.recreate();
-            this.mustRecreateSwapChain = false;
-        }
-
-        return this.swapChain;
-    }
 
     /**
      * Initializes the required context for rendering on the screen.
@@ -41,7 +28,42 @@ public final class RenderingContext implements AutoCloseable {
             final int windowWidth,
             final int windowHeight
     ) {
+        this.deviceContext = deviceContext;
+
         this.swapChain = new SwapChain(deviceContext, surface, windowWidth, windowHeight);
+        this.graphicsPipeline = new GraphicsPipeline();
+        this.framebuffers = new Framebuffers();
+        this.commandBuffers = new CommandBuffers();
+    }
+
+    /**
+     * Gets the swapchain and re-creates it in case the current instance has been invalidated. The
+     * result is considered invalid after GLFW events have been polled. Thus, the intended usage is
+     * to call get the swapchain once, immediately after polling the events:
+     * <pre><code>
+     *     glfwPollEvents();
+     *     final var swapchain = context.getSwapChain();
+     * </code></pre>
+     *
+     * @param windowWidth  mostly ignored, in case of swapchain re-creation, used to enforce the new
+     *                     window surface width
+     * @param windowHeight mostly ignored, in case of swapchain re-creation, used to enforce the new
+     *                     window surface height
+     *
+     * @return the current swapchain instance, which is guaranteed to be valid
+     */
+    public SwapChain getSwapChain(final int windowWidth, final int windowHeight) {
+        if (this.mustRecreateSwapChain) {
+            vkDeviceWaitIdle(this.deviceContext.getDevice());
+
+            this.swapChain.recreate(windowWidth, windowHeight);
+            //this.graphicsPipeline.recreate();
+            //this.frameBuffers.recreate();
+            //this.commandBuffers.recreate();
+            this.mustRecreateSwapChain = false;
+        }
+
+        return this.swapChain;
     }
 
     @Override
